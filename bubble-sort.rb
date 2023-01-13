33da1646c0f0ecd7565bf20b626a52db9e2e1f94
Sergio Guerra <sergioguerra6188@gmail.com>
2023-01-13 13:53:28 -0500

    Add code to bubble_sort method

---
+++ bubble-sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(unsorted_numbers)
+    length = unsorted_numbers.length
+    temp = 0
+    length.times do
+        unsorted_numbers.each_with_index do |value, index|
+            break if (index == (length - 1))
+            if ((unsorted_numbers[index] <=> unsorted_numbers[index + 1]) == 1)
+                temp = unsorted_numbers[index]
+                unsorted_numbers[index] = unsorted_numbers[index + 1]
+                unsorted_numbers[index + 1] = temp
+            end
+        end
+    end
+    return unsorted_numbers
+end
+
+p bubble_sort([4,3,78,2,0,2])
